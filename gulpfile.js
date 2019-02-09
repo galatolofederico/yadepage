3dee4dbd1dead4b7416a30921f5229ac2b155f88
Federico Galatolo <galatolo.federico@gmail.com>
2019-02-09 16:24:31 +0100

    argon2-browser exception in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -20,10 +20,29 @@ function generator(){
     .pipe(gulp.dest(buildDir+"/js"))
 }
 
-function copyLibs() {
+function html(){
+    return gulp.src(["src/html/**/*"])
+    .pipe(gulp.dest("./dist")) 
+}
+
+function config(){
+    return gulp.src(["src/config.js"])
+    .pipe(babel({
+    }))
+    .pipe(browserify({
+    }))
+    .pipe(gulp.dest("./dist"))
+}
+
+function libs() {
   return gulp.src(npmDist(), {base:'./node_modules'})
     .pipe(gulp.dest('./dist/libs'));
 }
 
-exports.build = gulp.series(clean, generator, copyLibs)
+function argon2(){
+    return gulp.src(["./node_modules/argon2-browser/**/*.js"])
+    .pipe(gulp.dest("./dist/libs/argon2-browser"))
+}
+
+exports.build = gulp.series(clean, generator, html, config, argon2, libs)
 exports.default = exports.build
