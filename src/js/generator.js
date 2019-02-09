3a5d7537e5486e8d70905ae19eb37381501c7fe2
Federico Galatolo <galatolo.federico@gmail.com>
2019-02-09 21:17:57 +0100

    Load configuration directly from config.js

--- src/js/generator.js
+++ src/js/generator.js
@@ -1,13 +1,11 @@
-const config = require("../config.js")
-
-function generator(masterPassword, login, useSymbols = false){
+function generator(masterPassword, login, config = {}){
     return new Promise(async (res, rej) => {
-        let masterHash = await getGenHash(masterPassword, config.generation.salt)
-        let salts = await getSalts(login)
-        let h1 = await getGenHash(masterHash, salts[0])
-        let h2 = await getGenHash(login, salts[1])
-        let h3 = await getGenHash(h1+h2, salts[2])
-        res(getPassword(h3, useSymbols))
+        let masterHash = await getGenHash(masterPassword, document.defaultConfig.generation.salt, config)
+        let salts = await getSalts(login, config)
+        let h1 = await getGenHash(masterHash, salts[0], config)
+        let h2 = await getGenHash(login, salts[1], config)
+        let h3 = await getGenHash(h1+h2, salts[2], config)
+        res(getPassword(h3, config.useSymbols || false))
     })
 }
 
@@ -21,27 +19,27 @@ function getPassword(hash, useSymbols){
     return str
 }
 
-async function getSalts(login){
+async function getSalts(login, config){
     let salts = []
     let currentArg = login
     for(let i = 0, salt = "";i < 3;i++){
-        salt = await getGenHash(currentArg, config.generation.salt)
+        salt = await getGenHash(currentArg, document.defaultConfig.generation.salt, config)
         salts.push(salt)
         currentArg = salt
     }
     return salts
 }
 
-function getGenHash(input, salt){
+function getGenHash(input, salt, config){
     return new Promise((res, rej) => {
         argon2.hash({
             pass: input,
             salt: salt,
-            time: config.generation.iterations,
-            mem: config.generation.memorySize, 
-            hashLen: config.generation.hashLengh,
+            time:  config.iterations || document.defaultConfig.generation.iterations,
+            mem:  config.memorySize || document.defaultConfig.generation.memorySize, 
+            hashLen: config.hashLengh || document.defaultConfig.generation.hashLengh,
             parallelism: 1,
-            type: config.generation.argonType,
+            type: config.argonType || document.defaultConfig.generation.argonType,
             distPath: 'libs/argon2-browser/dist'
         }).then(hash => {
             res(hash.hash)
