db22c553bf06d9be185823f24230e6bc6a72c961
Federico Galatolo <galatolo.federico@gmail.com>
2019-02-09 14:37:27 +0100

    gulpfile uses production libs

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ const gulp = require("gulp");
 const babel = require("gulp-babel");
 const browserify = require('gulp-browserify');
 const del = require("del")
+const npmDist = require('gulp-npm-dist');
+
+
 const buildDir = "./dist/"
 
 function clean(){
@@ -17,6 +20,10 @@ function generator(){
     .pipe(gulp.dest(buildDir+"/js"))
 }
 
+function copyLibs() {
+  return gulp.src(npmDist(), {base:'./node_modules'})
+    .pipe(gulp.dest('./dist/libs'));
+}
 
-exports.build = gulp.series(clean, generator)
+exports.build = gulp.series(clean, generator, copyLibs)
 exports.default = exports.build
